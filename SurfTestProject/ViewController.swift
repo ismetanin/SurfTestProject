48cd9522eeb67c3f24309e1d7ee7530435df0481
Ivan Smetanin <smetanin23@yandex.ru>
2016-09-08 17:45:42 +0300

    Initial commit.

--- SurfTestProject/ViewController.swift
+++ SurfTestProject/ViewController.swift
@@ -13,13 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myDict: NSDictionary?
+        if let path = NSBundle.mainBundle().pathForResource("input", ofType: "plist") {
+            myDict = NSDictionary(contentsOfFile: path)
+        }
+        if let dict = myDict {
+            for element in dict {
+                debugPrint(element.key)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
